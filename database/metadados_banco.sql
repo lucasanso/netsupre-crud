4b1e0acf9dd750df2a64bd40e6e5ecf44d1635e8
Lucas <lucasansodev@gmail.com>
2026-06-22 21:08:52 -0300

    feat: adiciona script SQL contendo os metadados do banco

--- database/metadados_banco.sql
+++ database/metadados_banco.sql
@@ -1,1 +1,23 @@
 -- Inicialização de Metadados
+
+CREATE TABLE IF NOT EXISTS pessoas (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    cpf VARCHAR(20) NOT NULL UNIQUE,
+    rg VARCHAR(20),
+    cep VARCHAR(15), 
+    logradouro VARCHAR(255),
+    complemento VARCHAR(100), 
+    setor VARCHAR(100),      
+    cidade VARCHAR(100),     
+    uf CHAR(2)                
+);
+
+CREATE TABLE IF NOT EXISTS telefones (
+    id SERIAL PRIMARY KEY,
+    pessoa_id INT NOT NULL,
+    numero VARCHAR(20) NOT NULL,
+    descricao VARCHAR(50),
+  
+    FOREIGN KEY (pessoa_id) REFERENCES pessoas(id) ON DELETE CASCADE
+);
